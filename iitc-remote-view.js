5fad9dc899a6a6f465b4cb44e2444f905db0d5cb
tmlai <1424491+tmlai@users.noreply.github.com>
2025-09-16 19:26:55 -0700

    Update iitc-remote-view.js
    Add function calculate distance to target port

--- iitc-remote-view.js
+++ iitc-remote-view.js
@@ -68,11 +68,36 @@ function wrapper(plugin_info) {
 
              // #resodetails
              linkDetails.append($('<aside>').append($('<div>').append(remoteViewHTML)));
-
+             var targetLat = 53.600212
+             var targetLong = -132.278533
              var lastTouched = '<div><span>Last touched: ' + new Date(portal.options.timestamp) + '</span></div>'
+             
+
+             let distance = haversine(targetLat, targetLong, lat, lng);
+		     //distance = distance.toFixed(1);
+
+             var distanceToTarget = '<div><span>Distance to target: ' +distance + '</span></div>'
+             
              linkDetails.append(lastTouched);
+             linkDetails.append(distanceToTarget);
          }, 0);
      }
+
+   //Credit: https://github.com/tehstone/IngressDronePath/blob/master/dronePathTravelPlanner.user.js#L1108
+   function haversine(lat1, lon1, lat2, lon2) {
+		const R = 6371e3; // metres
+		const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+		const φ2 = lat2 * Math.PI/180;
+		const Δφ = (lat2-lat1) * Math.PI/180;
+		const Δλ = (lon2-lon1) * Math.PI/180;
+
+		const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+					Math.cos(φ1) * Math.cos(φ2) *
+					Math.sin(Δλ/2) * Math.sin(Δλ/2);
+		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+		return R * c; // in metres
+	}
 
     window.makeRemoteView = function (latlng, guid, options) {
         options = options || {};
